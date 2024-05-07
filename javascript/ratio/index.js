9cebf18c2f6da40973010e60e02d4dbb4cd2ae23
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-05-07 17:45:10 +0900

    비율 계산 구현

---
+++ javascript/ratio/index.js
@@ -0,0 +1,9 @@
+// 비율 메소드
+// ex) 19 : 54 = n : m
+
+function ratio(a, b, n) {
+  return (n * b) / a;
+}
+
+const n = 375;
+console.log(ratio(19, 54, n));
---
+++ javascript/ratio/playground.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+const loop = Array(4)
+  .fill(0)
+  .map((_, i) => i + 1);
+
+const itemRef = ref();
+
+const itemH = ref(0);
+
+onMounted(() => {
+  console.log("item ref", itemRef.value);
+
+  const w1 = itemRef.value[0].offsetWidth;
+
+  const h1 = Math.floor(ratio(19, 54, w1));
+
+  console.log("아이템 높이", h1);
+
+  itemH.value = h1;
+});
+
+function ratio(a, b, n) {
+  return (n * b) / a;
+}
+</script>
+
+<template>
+  <div class="wrap">
+    <div ref="itemRef" class="wrap-item" v-for="item in loop" :key="item" :style="{ height: itemH + 'px' }">
+      <p class="wrap-p">{{ item }}</p>
+    </div>
+  </div>
+</template>
+
+<style>
+.wrap {
+  display: flex;
+  width: 1500px;
+
+  background-color: gray;
+}
+
+.wrap-item {
+  width: 25%;
+
+  padding: 10px;
+
+  border: 1px solid black;
+}
+
+.wrap-p {
+  background-color: white;
+}
+</style>
